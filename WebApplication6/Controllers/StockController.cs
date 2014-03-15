a785372c47fee08baca8c038935f1f1817ab4c18
olivier5741 <olivier5741@gmail.com>
2014-03-15 16:41:56 +0100

    Rossana

---
+++ WebApplication6/Controllers/StockController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication6.Controllers
+{
+    public class StockController : ApiController
+    {
+        // GET api/stock
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Salut Rossana", "value2" };
+        }
+
+        // GET api/stock/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/stock
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/stock/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/stock/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
