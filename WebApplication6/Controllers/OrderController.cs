115277e85608765b12a69d6573eac1f656187d34
Wouters Olivier <olivier5741@gmail.com>
2014-05-10 16:26:41 +0200

    RavenDB 1

--- WebApplication6/Controllers/OrderController.cs
+++ WebApplication6/Controllers/OrderController.cs
@@ -9,5 +9,38 @@ namespace WebApplication6.Controllers
 {
     public class OrderController : ApiController
     {
+        public IEnumerable<OrderProductDto> Get(string id)
+        {
+            return new List<OrderProductDto> { 
+                new OrderProductDto{
+                        Product = "Citron",
+                        Quantity = 10,
+                        Unit = "1",
+                        Price = 32.45m
+                    },
+                new OrderProductDto{
+                        Product = "Orange",
+                        Quantity = 24,
+                        Unit = "2",
+                        Price = 78.32m
+                    }
+            };
+        }
+
+        public HttpResponseMessage Post([FromBody]Object stockProduct)
+        {
+            if (stockProduct == null)
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+    }
+
+    public class OrderProductDto
+    {
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public decimal Price { get; set; }
     }
 }
--- WebApplication6/Global.asax.cs
+++ WebApplication6/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Raven.Client.Document;
+using Raven.Client.Indexes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,6 +14,8 @@ namespace WebApplication6
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static DocumentStore Store;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -18,6 +23,10 @@ namespace WebApplication6
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Store = new DocumentStore { ConnectionStringName = "RAVENHQ_CONNECTION_STRING" };
+            Store.Initialize();            
+            IndexCreation.CreateIndexes(Assembly.GetCallingAssembly(), Store);
         }
     }
 }
